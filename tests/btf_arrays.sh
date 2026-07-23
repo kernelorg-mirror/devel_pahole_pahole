acmel/dwarves

commit 472256d3c57bef53250686d55bd011f5dab4b657

btf_loader: Introduce a loader for the BTF format
Show 'struct list_head' from DWARF info:

  $ pahole -C list_head ~/git/build/v4.20-rc5+/net/ipv4/tcp.o
  struct list_head {
	  struct list_head *         next;                 /*     0     8 */
	  struct list_head *         prev;                 /*     8     8 */

	  /* size: 16, cachelines: 1, members: 2 */
	  /* last cacheline: 16 bytes */
  };

Try to show it from BTF, on a file without it:

  $ pahole -F btf -C list_head ~/git/build/v4.20-rc5+/net/ipv4/tcp.o
  pahole: /home/acme/git/build/v4.20-rc5+/net/ipv4/tcp.o: No debugging information found

Encode BTF from the DWARF info:

  $ pahole -J ~/git/build/v4.20-rc5+/net/ipv4/tcp.o

Check that it is there:
  $ readelf -SW ~/git/build/v4.20-rc5+/net/ipv4/tcp.o  | grep BTF
  readelf: /home/acme/git/build/v4.20-rc5+/net/ipv4/tcp.o: Warning: possibly corrupt ELF header - it has a non-zero program header offset, but no program headers
    [136] .BTF              PROGBITS        0000000000000000 101d0e 042edf 00      0   0  1

Now try again printing 'struct list_head' from the BTF info just
encoded:

  $ pahole -F btf -C list_head ~/git/build/v4.20-rc5+/net/ipv4/tcp.o  2> /dev/null
  struct list_head {
	  struct list_head *         next;                 /*     0     8 */
	  struct list_head *         prev;                 /*     8     8 */

	  /* size: 16, cachelines: 1, members: 2 */
	  /* last cacheline: 16 bytes */
  };
  $

There is the bitfields case that BTF desn't have the bit_size info for
bitfield members that makes the output from dwarf to be different than
the one from BTF:

  $ pahole -F btf -C sk_buff ~/git/build/v4.20-rc5+/net/ipv4/tcp.o > /tmp/sk_buff.btf
  $ pahole -F dwarf -C sk_buff ~/git/build/v4.20-rc5+/net/ipv4/tcp.o > /tmp/sk_buff.dwarf
  $ diff -u /tmp/sk_buff.dwarf /tmp/sk_buff.btf
  --- /tmp/sk_buff.dwarf	2018-12-20 14:50:51.428653046 -0300
  +++ /tmp/sk_buff.btf	2018-12-20 14:50:46.302601516 -0300
  @@ -38,45 +38,45 @@

btf_loader.c
@@ -1,0 +1,575 @@
+/* 
+ * btf_loader.c
+ *
+ * Copyright (C) 2018 Arnaldo Carvalho de Melo <acme@kernel.org>
+ *
+ * Based on ctf_loader.c that, in turn, was based on ctfdump.c: CTF dumper.
+ *
+ * Copyright (C) 2008 David S. Miller <davem@davemloft.net>
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <malloc.h>
+#include <string.h>
+#include <limits.h>
+#include <libgen.h>
+#include <zlib.h>
+
+#include <gelf.h>
+
+#include "libbtf.h"
+#include "btf.h"
+#include "dutil.h"
+#include "dwarves.h"
+
+/*
+ * FIXME: We should just get the table from the BTF ELF section
+ * and use it directly
+ */
+extern struct strings *strings;
+
+static void *tag__alloc(const size_t size)
+{
+	struct tag *tag = zalloc(size);
+
+	if (tag != NULL)
+		tag->top_level = 1;
+
+	return tag;
+}
+
+static int btf__load_ftype(struct btf *btf, struct ftype *proto, uint16_t tag,
+			   uint32_t type, uint16_t vlen, struct btf_param *args, long id)
+{
+	int i;
+
+	proto->tag.tag	= tag;
+	proto->tag.type = type;
+	INIT_LIST_HEAD(&proto->parms);
+
+	for (i = 0; i < vlen; ++i) {
+		struct btf_param param = {
+		       .name_off = btf__get32(btf, &args[i].name_off),
+		       .type	 = btf__get32(btf, &args[i].type),
+		};
+
+		if (param.type == 0)
+			proto->unspec_parms = 1;
+		else {
+			struct parameter *p = tag__alloc(sizeof(*p));
+
+			if (p == NULL)
+				goto out_free_parameters;
+			p->tag.tag  = DW_TAG_formal_parameter;
+			p->tag.type = param.type;
+			p->name	    = param.name_off;
+			ftype__add_parameter(proto, p);
+		}
+	}
+
+	vlen *= sizeof(*args);
+	cu__add_tag(btf->priv, &proto->tag, &id);
+
+	return vlen;
+out_free_parameters:
+	ftype__delete(proto, btf->priv);
+	return -ENOMEM;
+}
+
+
+static struct base_type *base_type__new(strings_t name, uint32_t attrs,
+					uint8_t float_type, size_t size)
+{
+        struct base_type *bt = tag__alloc(sizeof(*bt));
+
+	if (bt != NULL) {
+		bt->name = name;
+		bt->bit_size = size;
+		bt->is_signed = attrs & BTF_INT_SIGNED;
+		bt->is_bool = attrs & BTF_INT_BOOL;
+		bt->name_has_encoding = false;
+		bt->float_type = float_type;
+	}
+	return bt;
+}
+
+static void type__init(struct type *type, uint16_t tag,
+		       strings_t name, size_t size)
+{
+	INIT_LIST_HEAD(&type->node);
+	INIT_LIST_HEAD(&type->namespace.tags);
+	type->size = size;
+	type->namespace.tag.tag = tag;
+	type->namespace.name = name;
+	type->namespace.sname = 0;
+}
+
+static struct type *type__new(uint16_t tag, strings_t name, size_t size)
+{
+        struct type *type = tag__alloc(sizeof(*type));
+
+	if (type != NULL)
+		type__init(type, tag, name, size);
+
+	return type;
+}
+
+static struct class *class__new(strings_t name, size_t size)
+{
+	struct class *class = tag__alloc(sizeof(*class));
+
+	if (class != NULL) {
+		type__init(&class->type, DW_TAG_structure_type, name, size);
+		INIT_LIST_HEAD(&class->vtable);
+	}
+
+	return class;
+}
+
+static int create_new_base_type(struct btf *btf, void *ptr, struct btf_type *tp, long id)
+{
+	uint32_t *enc = ptr;
+	uint32_t eval = btf__get32(btf, enc);
+	uint32_t attrs = BTF_INT_ENCODING(eval);
+	strings_t name = btf__get32(btf, &tp->name_off);
+	struct base_type *base = base_type__new(name, attrs, 0,
+						BTF_INT_BITS(eval));
+	if (base == NULL)
+		return -ENOMEM;
+
+	base->tag.tag = DW_TAG_base_type;
+	cu__add_tag(btf->priv, &base->tag, &id);
+
+	return sizeof(*enc);
+}
+
+static int create_new_array(struct btf *btf, void *ptr, long id)
+{
+	struct btf_array *ap = ptr;
+	struct array_type *array = tag__alloc(sizeof(*array));
+
+	if (array == NULL)
+		return -ENOMEM;
+
+	/* FIXME: where to get the number of dimensions?
+	 * it it flattened? */
+	array->dimensions = 1;
+	array->nr_entries = malloc(sizeof(uint32_t));
+
+	if (array->nr_entries == NULL) {
+		free(array);
+		return -ENOMEM;
+	}
+
+	array->nr_entries[0] = btf__get32(btf, &ap->nelems);
+	array->tag.tag = DW_TAG_array_type;
+	array->tag.type = btf__get32(btf, &ap->type);
+
+	cu__add_tag(btf->priv, &array->tag, &id);
+
+	return sizeof(*ap);
+}
+
+static int create_members(struct btf *btf, void *ptr, int vlen, struct type *class)
+{
+	struct btf_member *mp = ptr;
+	int i;
+
+	for (i = 0; i < vlen; i++) {
+		struct class_member *member = zalloc(sizeof(*member));
+		uint32_t offset;
+
+		if (member == NULL)
+			return -ENOMEM;
+
+		member->tag.tag    = DW_TAG_member;
+		member->tag.type   = btf__get32(btf, &mp[i].type);
+		member->name	   = btf__get32(btf, &mp[i].name_off);
+		offset = btf__get32(btf, &mp[i].offset);
+		member->bit_offset = BTF_MEMBER_BIT_OFFSET(offset);
+		member->bitfield_size = BTF_MEMBER_BITFIELD_SIZE(offset);
+		/* sizes and offsets will be corrected at class__fixup_btf_bitfields */
+		type__add_member(class, member);
+	}
+
+	return sizeof(*mp);
+}
+
+static int create_new_class(struct btf *btf, void *ptr, int vlen, struct btf_type *tp, uint64_t size, long id)
+{
+	strings_t name = btf__get32(btf, &tp->name_off);
+	struct class *class = class__new(name, size);
+	int member_size = create_members(btf, ptr, vlen, &class->type);
+
+	if (member_size < 0)
+		goto out_free;
+
+	cu__add_tag(btf->priv, &class->type.namespace.tag, &id);
+
+	return (vlen * member_size);
+out_free:
+	class__delete(class, btf->priv);
+	return -ENOMEM;
+}
+
+static int create_new_union(struct btf *btf, void *ptr,
+			    int vlen, struct btf_type *tp,
+			    uint64_t size, long id)
+{
+	strings_t name = btf__get32(btf, &tp->name_off);
+	struct type *un = type__new(DW_TAG_union_type, name, size);
+	int member_size = create_members(btf, ptr, vlen, un);
+
+	if (member_size < 0)
+		goto out_free;
+
+	cu__add_tag(btf->priv, &un->namespace.tag, &id);
+
+	return (vlen * member_size);
+out_free:
+	type__delete(un, btf->priv);
+	return -ENOMEM;
+}
+
+static struct enumerator *enumerator__new(strings_t name, uint32_t value)
+{
+	struct enumerator *en = tag__alloc(sizeof(*en));
+
+	if (en != NULL) {
+		en->name = name;
+		en->value = value;
+		en->tag.tag = DW_TAG_enumerator;
+	}
+
+	return en;
+}
+
+static int create_new_enumeration(struct btf *btf, void *ptr,
+				  int vlen, struct btf_type *tp,
+				  uint16_t size, long id)
+{
+	struct btf_enum *ep = ptr;
+	uint16_t i;
+	struct type *enumeration = type__new(DW_TAG_enumeration_type,
+					     btf__get32(btf, &tp->name_off),
+					     size ?: (sizeof(int) * 8));
+
+	if (enumeration == NULL)
+		return -ENOMEM;
+
+	for (i = 0; i < vlen; i++) {
+		strings_t name = btf__get32(btf, &ep[i].name_off);
+		uint32_t value = btf__get32(btf, &ep[i].val);
+		struct enumerator *enumerator = enumerator__new(name, value);
+
+		if (enumerator == NULL)
+			goto out_free;
+
+		enumeration__add(enumeration, enumerator);
+	}
+
+	cu__add_tag(btf->priv, &enumeration->namespace.tag, &id);
+
+	return (vlen * sizeof(*ep));
+out_free:
+	enumeration__delete(enumeration, btf->priv);
+	return -ENOMEM;
+}
+
+static int create_new_subroutine_type(struct btf *btf, void *ptr,
+				      int vlen, struct btf_type *tp,
+				      long id)
+{
+	struct btf_param *args = ptr;
+	unsigned int type = btf__get32(btf, &tp->type);
+	struct ftype *proto = tag__alloc(sizeof(*proto));
+
+	if (proto == NULL)
+		return -ENOMEM;
+
+	vlen = btf__load_ftype(btf, proto, DW_TAG_subroutine_type,
+			       type, vlen, args, id);
+	return vlen < 0 ? -ENOMEM : vlen;
+}
+
+static int create_new_forward_decl(struct btf *btf, struct btf_type *tp, uint64_t size, long id)
+{
+	strings_t name = btf__get32(btf, &tp->name_off);
+	struct class *fwd = class__new(name, size);
+
+	if (fwd == NULL)
+		return -ENOMEM;
+	fwd->type.declaration = 1;
+	cu__add_tag(btf->priv, &fwd->type.namespace.tag, &id);
+	return 0;
+}
+
+static int create_new_typedef(struct btf *btf, struct btf_type *tp, uint64_t size, long id)
+{
+	strings_t name = btf__get32(btf, &tp->name_off);
+	unsigned int type_id = btf__get32(btf, &tp->type);
+	struct type *type = type__new(DW_TAG_typedef, name, size);
+
+	if (type == NULL)
+		return -ENOMEM;
+
+	type->namespace.tag.type = type_id;
+	cu__add_tag(btf->priv, &type->namespace.tag, &id);
+
+	return 0;
+}
+
+static int create_new_tag(struct btf *btf, int type, struct btf_type *tp, long id)
+{
+	unsigned int type_id = btf__get32(btf, &tp->type);
+	struct tag *tag = zalloc(sizeof(*tag));
+
+	if (tag == NULL)
+		return -ENOMEM;
+
+	switch (type) {
+	case BTF_KIND_CONST:	tag->tag = DW_TAG_const_type;	 break;
+	case BTF_KIND_PTR:	tag->tag = DW_TAG_pointer_type;  break;
+	case BTF_KIND_RESTRICT:	tag->tag = DW_TAG_restrict_type; break;
+	case BTF_KIND_VOLATILE:	tag->tag = DW_TAG_volatile_type; break;
+	default:
+		printf("%s: FOO %d\n\n", __func__, type);
+		return 0;
+	}
+
+	tag->type = type_id;
+	cu__add_tag(btf->priv, tag, &id);
+
+	return 0;
+}
+
+void *btf__get_buffer(struct btf *btf)
+{
+	return btf->data;
+}
+
+size_t btf__get_size(struct btf *btf)
+{
+	return btf->size;
+}
+
+static int btf__load_types(struct btf *btf)
+{
+	void *btf_buffer = btf__get_buffer(btf);
+	struct btf_header *hp = btf_buffer;
+	void *btf_contents = btf_buffer + sizeof(*hp),
+	     *type_section = (btf_contents + btf__get32(btf, &hp->type_off)),
+	     *strings_section = (btf_contents + btf__get32(btf, &hp->str_off));
+	struct btf_type *type_ptr = type_section,
+			*end = strings_section;
+	unsigned int type_index = 0x0001;
+
+	while (type_ptr < end) {
+		uint32_t val  = btf__get32(btf, &type_ptr->info);
+		uint32_t type = BTF_INFO_KIND(val);
+		int	 vlen = BTF_INFO_VLEN(val);
+		void	 *ptr = type_ptr;
+		uint32_t size = btf__get32(btf, &type_ptr->size);
+
+		ptr += sizeof(struct btf_type);
+
+		if (type == BTF_KIND_INT) {
+			vlen = create_new_base_type(btf, ptr, type_ptr, type_index);
+		} else if (type == BTF_KIND_ARRAY) {
+			vlen = create_new_array(btf, ptr, type_index);
+		} else if (type == BTF_KIND_STRUCT) {
+			vlen = create_new_class(btf, ptr, vlen, type_ptr, size, type_index);
+		} else if (type == BTF_KIND_UNION) {
+			vlen = create_new_union(btf, ptr, vlen, type_ptr, size, type_index);
+		} else if (type == BTF_KIND_ENUM) {
+			vlen = create_new_enumeration(btf, ptr, vlen, type_ptr, size, type_index);
+		} else if (type == BTF_KIND_FWD) {
+			vlen = create_new_forward_decl(btf, type_ptr, size, type_index);
+		} else if (type == BTF_KIND_TYPEDEF) {
+			vlen = create_new_typedef(btf, type_ptr, size, type_index);
+		} else if (type == BTF_KIND_VOLATILE ||
+			   type == BTF_KIND_PTR ||
+			   type == BTF_KIND_CONST ||
+			   type == BTF_KIND_RESTRICT) {
+			vlen = create_new_tag(btf, type, type_ptr, type_index);
+		} else if (type == BTF_KIND_UNKN) {
+			cu__table_nullify_type_entry(btf->priv, type_index);
+			fprintf(stderr,
+				"BTF: idx: %d, off: %zd, Unknown\n",
+				type_index, ((void *)type_ptr) - type_section);
+			fflush(stderr);
+			vlen = 0;
+		} else if (type == BTF_KIND_FUNC_PROTO) {
+			vlen = create_new_subroutine_type(btf, ptr, vlen, type_ptr, type_index);
+		} else {
+			fprintf(stderr,
+				"BTF: idx: %d, off: %zd, Unknown\n",
+				type_index, ((void *)type_ptr) - type_section);
+			fflush(stderr);
+			vlen = 0;
+		}
+
+		if (vlen < 0)
+			return vlen;
+
+		type_ptr = ptr + vlen;
+		type_index++;
+	}
+	return 0;
+}
+
+static int btf__load_sections(struct btf *btf)
+{
+	return btf__load_types(btf);
+}
+
+static int class__fixup_btf_bitfields(struct tag *tag, struct cu *cu)
+{
+	struct class_member *pos;
+	struct type *tag_type = tag__type(tag);
+
+	type__for_each_data_member(tag_type, pos) {
+		struct tag *type = tag__follow_typedef(&pos->tag, cu);
+
+		if (type == NULL) /* FIXME: C++ BTF... */
+			continue;
+
+		pos->bitfield_offset = 0;
+		pos->bitfield_size = 0;
+		pos->byte_offset = pos->bit_offset / 8;
+
+		uint16_t type_bit_size;
+		size_t integral_bit_size;
+
+		switch (type->tag) {
+		case DW_TAG_enumeration_type:
+			type_bit_size = tag__type(type)->size;
+			/* Best we can do to check if this is a packed enum */
+			if (is_power_of_2(type_bit_size))
+				integral_bit_size = roundup(type_bit_size, 8);
+			else
+				integral_bit_size = sizeof(int) * 8;
+			break;
+		case DW_TAG_base_type: {
+			struct base_type *bt = tag__base_type(type);
+			char name[256];
+			type_bit_size = bt->bit_size;
+			integral_bit_size = base_type__name_to_size(bt, cu);
+			if (integral_bit_size == 0) {
+				fprintf(stderr, "%s: unknown base type name \"%s\"!\n",
+					__func__, base_type__name(bt, cu, name,
+								  sizeof(name)));
+			}
+		}
+			break;
+		default:
+			pos->byte_size = tag__size(type, cu);
+			pos->bit_size = pos->byte_size * 8;
+			continue;
+		}
+
+		/*
+		 * XXX: integral_bit_size can be zero if base_type__name_to_size doesn't
+		 * know about the base_type name, so one has to add there when
+		 * such base_type isn't found. pahole will put zero on the
+		 * struct output so it should be easy to spot the name when
+		 * such unlikely thing happens.
+		 */
+		pos->byte_size = integral_bit_size / 8;
+
+		if (integral_bit_size == 0 || type_bit_size == integral_bit_size) {
+			pos->bit_size = integral_bit_size;
+			continue;
+		}
+
+		pos->bitfield_offset = pos->bit_offset % integral_bit_size;
+		pos->bitfield_size = type_bit_size;
+		pos->bit_size = type_bit_size;
+		pos->byte_offset = (((pos->bit_offset / integral_bit_size) *
+				     integral_bit_size) / 8);
+	}
+
+	return 0;
+}
+
+static int cu__fixup_btf_bitfields(struct cu *cu)
+{
+	int err = 0;
+	struct tag *pos;
+
+	list_for_each_entry(pos, &cu->tags, node)
+		if (tag__is_struct(pos) || tag__is_union(pos)) {
+			err = class__fixup_btf_bitfields(pos, cu);
+			if (err)
+				break;
+		}
+
+	return err;
+}
+
+static void btf__cu_delete(struct cu *cu)
+{
+	btf__free(cu->priv);
+	cu->priv = NULL;
+}
+
+static const char *btf__strings_ptr(const struct cu *cu, strings_t s)
+{
+	return btf__string(cu->priv, s);
+}
+
+struct debug_fmt_ops btf__ops;
+
+int btf__load_file(struct cus *cus, struct conf_load *conf,
+		   const char *filename)
+{
+	int err;
+	struct btf *state = btf__new(filename, NULL);
+
+	if (state == NULL)
+		return -1;
+
+	struct cu *cu = cu__new(filename, state->wordsize, NULL, 0, filename);
+	if (cu == NULL)
+		return -1;
+
+	cu->language = LANG_C;
+	cu->uses_global_strings = false;
+	cu->dfops = &btf__ops;
+	cu->priv = state;
+	state->priv = cu;
+	if (btf__load(state) != 0)
+		return -1;
+
+	err = btf__load_sections(state);
+
+	if (err != 0) {
+		cu__delete(cu);
+		return err;
+	}
+
+	err = cu__fixup_btf_bitfields(cu);
+	/*
+	 * The app stole this cu, possibly deleting it,
+	 * so forget about it
+	 */
+	if (conf && conf->steal && conf->steal(cu, conf))
+		return 0;
+
+	cus__add(cus, cu);
+	return err;
+}
+
+struct debug_fmt_ops btf__ops = {
+	.name		= "btf",
+	.load_file	= btf__load_file,
+	.strings__ptr	= btf__strings_ptr,
+	.cu__delete	= btf__cu_delete,
+};

CMakeLists.txt
@@ -34,7 +34,7 @@ find_package(ZLIB REQUIRED)
 _set_fancy(LIB_INSTALL_DIR "${EXEC_INSTALL_PREFIX}${CMAKE_INSTALL_PREFIX}/${__LIB}" "libdir")
 
 set(dwarves_LIB_SRCS dwarves.c dwarves_fprintf.c gobuffer strings
-		     ctf_encoder.c ctf_loader.c libctf.c btf_encoder.c libbtf.c
+		     ctf_encoder.c ctf_loader.c libctf.c btf_encoder.c btf_loader.c libbtf.c
 		     dwarf_loader.c dutil.c elf_symtab.c rbtree.c)
 add_library(dwarves SHARED ${dwarves_LIB_SRCS})
 set_target_properties(dwarves PROPERTIES VERSION 1.0.0 SOVERSION 1)

dwarves.c
@@ -1453,11 +1453,12 @@ out:
 /*
  * This should really do demand loading of DSOs, STABS anyone? 8-)
  */
-extern struct debug_fmt_ops dwarf__ops, ctf__ops;
+extern struct debug_fmt_ops dwarf__ops, ctf__ops, btf__ops;
 
 static struct debug_fmt_ops *debug_fmt_table[] = {
 	&dwarf__ops,
 	&ctf__ops,
+	&btf__ops,
 	NULL,
 };
 

libbtf.c
@@ -37,6 +37,57 @@ struct btf_array_type {
 
 uint8_t btf_verbose;
 
+uint32_t btf__get32(struct btf *btf, uint32_t *p)
+{
+	uint32_t val = *p;
+
+	if (btf->swapped)
+		val = ((val >> 24) |
+		       ((val >> 8) & 0x0000ff00) |
+		       ((val << 8) & 0x00ff0000) |
+		       (val << 24));
+	return val;
+}
+
+int btf__load(struct btf *btf)
+{
+	int err = -ENOTSUP;
+	GElf_Shdr shdr;
+	Elf_Scn *sec = elf_section_by_name(btf->elf, &btf->ehdr, &shdr, ".BTF", NULL);
+
+	if (sec == NULL)
+		return -ESRCH;
+
+	Elf_Data *data = elf_getdata(sec, NULL);
+	if (data == NULL) {
+		fprintf(stderr, "%s: cannot get data of BTF section.\n", __func__);
+		return -1;
+	}
+
+	struct btf_header *hp = data->d_buf;
+	size_t orig_size = data->d_size;
+
+	if (hp->version != BTF_VERSION)
+		goto out;
+
+	err = -EINVAL;
+	if (hp->magic == BTF_MAGIC)
+		btf->swapped = 0;
+	else
+		goto out;
+
+	err = -ENOMEM;
+	btf->data = malloc(orig_size);
+	if (btf->data != NULL) {
+		memcpy(btf->data, hp, orig_size);
+		btf->size = orig_size;
+		err = 0;
+	}
+out:
+	return err;
+}
+
+
 struct btf *btf__new(const char *filename, Elf *elf)
 {
 	struct btf *btf = zalloc(sizeof(*btf));
@@ -111,6 +162,23 @@ void btf__free(struct btf *btf)
 	free(btf->filename);
 	free(btf->data);
 	free(btf);
+}
+
+char *btf__string(struct btf *btf, uint32_t ref)
+{
+	struct btf_header *hp = btf->hdr;
+	uint32_t off = ref;
+	char *name;
+
+	if (off >= btf__get32(btf, &hp->str_len))
+		return "(ref out-of-bounds)";
+
+	if ((off + btf__get32(btf, &hp->str_off)) >= btf->size)
+		return "(string table truncated)";
+
+	name = ((char *)(hp + 1) + btf__get32(btf, &hp->str_off) + off);
+
+	return name[0] == '\0' ? NULL : name;
 }
 
 static void *btf__nohdr_data(struct btf *btf)

libbtf.h
@@ -51,4 +51,13 @@ int32_t btf__add_func_proto(struct btf *btf, struct ftype *ftype,
 void btf__set_strings(struct btf *btf, struct gobuffer *strings);
 int  btf__encode(struct btf *btf, uint8_t flags);
 
+char *btf__string(struct btf *btf, uint32_t ref);
+int btf__load(struct btf *btf);
+
+uint32_t btf__get32(struct btf *btf, uint32_t *p);
+
+void *btf__get_buffer(struct btf *btf);
+
+size_t btf__get_size(struct btf *btf);
+
 #endif /* _LIBBTF_H */

kernelorg-mirror/devel_pahole_pahole

commit 74ff6de5f0b68c43179b143b4c0e3d1876ec4265

btf_loader: Fix multi-dimensional array loading
create_new_array() had a FIXME — it loaded every BTF_KIND_ARRAY as a
1D array (dimensions=1, nr_entries=[nelems]) and lost the chaining
that encodes multi-dim arrays.

BTF chains dimensions through the element type field:
  inner: ARRAY { type=int,   nelems=4 }  ← int[4]
  outer: ARRAY { type=inner, nelems=3 }  ← int[3][4]

When the outer node is loaded, the inner has already been loaded (BTF
types are processed in id order, and inner always has a lower id).
Detect this by checking whether cu__type(cu, ap->type) is a
DW_TAG_array_type, and if so, absorb its dimensions:

  array->dimensions   = inner->dimensions + 1
  array->nr_entries[] = [ap->nelems, inner->nr_entries...]
  array->tag.type     = inner->tag.type   ← skip to base element type

Note: cu__type() (types_table) must be used, not cu__tag() (tags_table);
DW_TAG_array_type entries are stored in the types table.

Before: int a[3][4] round-tripped as int a[4][3] (reversed dimensions,
        because each BTF_KIND_ARRAY was 1D and the display chain printed
        dimensions from innermost to outermost)
After:  int a[3][4] and int a[2][3][4] round-trip correctly via BTF

Fixes: 472256d3c57bef53 ("btf_loader: Introduce a loader for the BTF format")
Assisted-by: Claude:claude-sonnet-4-6
Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

btf_loader.c
@@ -201,20 +201,46 @@ static int create_new_array(struct cu *cu, const struct btf_type *tp, uint32_t i
 	if (array == NULL)
 		return -ENOMEM;
 
-	/* FIXME: where to get the number of dimensions?
-	 * it it flattened? */
-	array->dimensions = 1;
-	array->nr_entries = malloc(sizeof(uint32_t));
+	/*
+	 * BTF encodes each dimension of a multi-dimensional array as a
+	 * separate BTF_KIND_ARRAY node chained via the element type field.
+	 * For example, int a[3][4] becomes:
+	 *   inner: BTF_KIND_ARRAY { type=int,   nelems=4 }
+	 *   outer: BTF_KIND_ARRAY { type=inner, nelems=3 }
+	 *
+	 * Reconstruct the pahole multi-dim representation (one array_type
+	 * with dimensions[] and nr_entries[]) by absorbing any inner
+	 * BTF_KIND_ARRAY that was already loaded.
+	 */
+	struct tag *elem_tag = cu__type(cu, ap->type);
 
-	if (array->nr_entries == NULL) {
-		free(array);
-		return -ENOMEM;
+	if (elem_tag && elem_tag->tag == DW_TAG_array_type &&
+	    tag__array_type(elem_tag)->dimensions < UINT8_MAX) {
+		struct array_type *inner = tag__array_type(elem_tag);
+
+		array->dimensions = inner->dimensions + 1;
+		array->nr_entries = malloc(array->dimensions * sizeof(uint32_t));
+		if (array->nr_entries == NULL) {
+			free(array);
+			return -ENOMEM;
+		}
+		array->nr_entries[0] = ap->nelems;
+		memcpy(&array->nr_entries[1], inner->nr_entries,
+		       inner->dimensions * sizeof(uint32_t));
+		/* point directly to the base element type, skipping the inner array node */
+		array->tag.type = inner->tag.type;
+	} else {
+		array->dimensions = 1;
+		array->nr_entries = malloc(sizeof(uint32_t));
+		if (array->nr_entries == NULL) {
+			free(array);
+			return -ENOMEM;
+		}
+		array->nr_entries[0] = ap->nelems;
+		array->tag.type = ap->type;
 	}
 
-	array->nr_entries[0] = ap->nelems;
 	array->tag.tag = DW_TAG_array_type;
-	array->tag.type = ap->type;
-
 	cu__add_tag_with_id(cu, &array->tag, id);
 
 	return 0;

tests/btf_arrays.sh
@@ -1,0 +1,158 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright © 2026 Red Hat Inc, Arnaldo Carvalho de Melo <acme@redhat.com>
+#
+# Test BTF multi-dimensional array encoding and loading round-trip.
+#
+# BTF represents each dimension of a multi-dimensional array as a separate
+# chained BTF_KIND_ARRAY node.  For int a[3][4]:
+#   inner: ARRAY { type=int,   nelems=4 }
+#   outer: ARRAY { type=inner, nelems=3 }
+#
+# The encoder (btf_encoder.c) must emit one BTF_KIND_ARRAY per dimension,
+# from innermost to outermost.  The loader (btf_loader.c) must collapse
+# the chain back into pahole's flat multi-dim array_type.
+#
+# Tests:
+#  1. BTF dump has the right number of chained ARRAY nodes for each dim
+#  2. pahole -F btf output matches DWARF output for all array members
+#  3. Array sizes are preserved across the encode→load round-trip
+
+. "$(dirname "$0")/test_lib.sh"
+
+outdir=$(make_tmpdir)
+trap cleanup EXIT
+
+title_log "BTF multi-dimensional array encoding and round-trip."
+
+CC=${CC:-gcc}
+if ! command -v ${CC%% *} > /dev/null 2>&1; then
+	info_log "skip: $CC not available"
+	test_skip
+fi
+
+if ! command -v bpftool > /dev/null 2>&1; then
+	info_log "skip: bpftool not available"
+	test_skip
+fi
+
+src=$(make_tmpsrc)
+obj=$(make_tmpobj)
+
+cat > "$src" << 'EOF'
+struct multidim {
+	int a1d[5];
+	int a2d[3][4];
+	int a3d[2][3][4];
+	char s[8];
+};
+
+struct multidim g;
+EOF
+
+$CC -g -c -o "$obj" "$src" 2>/dev/null
+if [ $? -ne 0 ]; then
+	error_log "FAIL: compilation failed"
+	test_fail
+fi
+
+# --- BTF encoding ---
+
+btf="$outdir/arrays.btf"
+pahole --btf_encode_detached="$btf" "$obj" 2>/dev/null
+if [ $? -ne 0 ] || [ ! -s "$btf" ]; then
+	error_log "FAIL: pahole --btf_encode_detached failed"
+	test_fail
+fi
+
+dump=$(bpftool btf dump file "$btf" 2>/dev/null)
+if [ -z "$dump" ]; then
+	error_log "FAIL: bpftool btf dump produced no output"
+	test_fail
+fi
+
+# --- Check 1: chained ARRAY nodes for int[3][4] ---
+# After btf__dedup the chain int[4] → int[3][4] must be present.
+
+inner4_id=$(echo "$dump" | grep "ARRAY.*nr_elems=4" | head -1 | sed 's/\[\([0-9]*\)\].*/\1/')
+if [ -z "$inner4_id" ]; then
+	error_log "FAIL: no ARRAY with nr_elems=4 found for int[3][4] inner dim"
+	test_fail
+fi
+info_log "inner ARRAY nr_elems=4 at type_id=$inner4_id: ok"
+
+# There must be an ARRAY referencing inner4_id with nr_elems=3 (outer of int[3][4])
+if ! echo "$dump" | grep -q "ARRAY.*type_id=${inner4_id}.*nr_elems=3"; then
+	error_log "FAIL: no outer ARRAY(type=${inner4_id}, nr_elems=3) for int[3][4]"
+	test_fail
+fi
+info_log "outer ARRAY nr_elems=3 referencing inner: ok"
+
+# --- Check 2: struct member type_ids must differ between a2d and a3d ---
+# If they were the same, a3d would not add the extra outer dimension.
+
+a2d_type=$(echo "$dump" | grep "'a2d'" | sed "s/.*type_id=\([0-9]*\).*/\1/")
+a3d_type=$(echo "$dump" | grep "'a3d'" | sed "s/.*type_id=\([0-9]*\).*/\1/")
+
+if [ -z "$a2d_type" ] || [ -z "$a3d_type" ]; then
+	error_log "FAIL: could not extract type_ids for a2d ($a2d_type) or a3d ($a3d_type)"
+	test_fail
+fi
+if [ "$a2d_type" = "$a3d_type" ]; then
+	error_log "FAIL: a2d and a3d share the same type_id ($a2d_type); a3d not encoded with extra dim"
+	test_fail
+fi
+info_log "a2d type_id=$a2d_type, a3d type_id=$a3d_type (distinct): ok"
+
+# --- Check 3: round-trip via pahole -F btf ---
+# In-place encoding then reload via BTF frontend must match DWARF output.
+
+pahole -J "$obj" 2>/dev/null
+if [ $? -ne 0 ]; then
+	error_log "FAIL: pahole -J in-place encoding failed"
+	test_fail
+fi
+
+btf_out=$(pahole -F btf -C multidim "$obj" 2>/dev/null)
+
+if [ -z "$btf_out" ]; then
+	error_log "FAIL: pahole -F btf -C multidim produced no output"
+	test_fail
+fi
+
+# 1D array preserved
+if ! echo "$btf_out" | grep -q "a1d\[5\]"; then
+	error_log "FAIL: round-trip: a1d[5] not found in BTF output"
+	test_fail
+fi
+info_log "a1d[5] preserved: ok"
+
+# 2D array — exact dimension order must match DWARF
+if ! echo "$btf_out" | grep -q "a2d\[3\]\[4\]"; then
+	error_log "FAIL: round-trip: a2d[3][4] not found in BTF output"
+	test_fail
+fi
+info_log "a2d[3][4] preserved: ok"
+
+# 3D array — exact dimension order must match DWARF
+if ! echo "$btf_out" | grep -q "a3d\[2\]\[3\]\[4\]"; then
+	error_log "FAIL: round-trip: a3d[2][3][4] not found in BTF output"
+	test_fail
+fi
+info_log "a3d[2][3][4] preserved: ok"
+
+# char array preserved
+if ! echo "$btf_out" | grep -q "s\[8\]"; then
+	error_log "FAIL: round-trip: s[8] not found in BTF output"
+	test_fail
+fi
+info_log "s[8] preserved: ok"
+
+# --- Check 4: struct size preserved ---
+if ! echo "$btf_out" | grep -q "size: 172"; then
+	error_log "FAIL: struct multidim size=172 not preserved after BTF round-trip"
+	test_fail
+fi
+info_log "struct multidim size=172 preserved: ok"
+
+test_pass